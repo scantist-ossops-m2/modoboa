0079ea6295a9e8b8246e2b33cdcd648447d6ec54
TL-Yao <tongle@scntist.com>
2024-08-06 15:20:49 +0000

    commit patch 15214804

---
+++ build/lib/modoboa/core/tests/stupid_extension_2/urls.py
@@ -0,0 +1,11 @@
+"""Custom urls."""
+
+from __future__ import unicode_literals
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url("^$", views.test_view, name="index")
+]
